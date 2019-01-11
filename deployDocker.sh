4e3b4d5c5b70cf5384a0bd71214f2179d1c70dd3
naraku0412 <meilongyx@163.com>
2019-01-11 10:26:29 +0800

    add: add some .sh

---
+++ deployDocker.sh
@@ -0,0 +1,85 @@
+#! /bin/bash
+
+cd /home/k8s-install
+source /usr/local/bin/environment.sh
+
+# 部署worker节点
+for node_ip in ${NODE_IPS[@]}
+  do
+    echo ">>> ${node_ip}"
+    ssh root@${node_ip} "yum install -y epel-release"
+    ssh root@${node_ip} "yum install -y conntrack ipvsadm ipset jq iptables curl sysstat libseccomp && /usr/sbin/modprobe ip_vs "
+  done
+
+# 部署docker组件
+# 下载和分发 docker 二进制文件
+wget https://download.docker.com/linux/static/stable/x86_64/docker-18.03.1-ce.tgz
+tar -xvf docker-18.03.1-ce.tgz
+
+for node_ip in ${NODE_IPS[@]}
+  do
+    echo ">>> ${node_ip}"
+    scp docker/docker*  root@${node_ip}:/usr/local/bin/
+    ssh root@${node_ip} "chmod +x /usr/local/bin/*"
+  done
+
+# 创建和分发systemd unit文件
+cat > docker.service <<"EOF"
+[Unit]
+Description=Docker Application Container Engine
+Documentation=http://docs.docker.io
+
+[Service]
+Environment="PATH=/usr/local/bin:/bin:/sbin:/usr/bin:/usr/sbin"
+EnvironmentFile=-/run/flannel/docker
+ExecStart=/usr/local/bin/dockerd --log-level=error $DOCKER_NETWORK_OPTIONS
+ExecReload=/bin/kill -s HUP $MAINPID
+Restart=on-failure
+RestartSec=5
+LimitNOFILE=infinity
+LimitNPROC=infinity
+LimitCORE=infinity
+Delegate=yes
+KillMode=process
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+for node_ip in ${NODE_IPS[@]}
+  do echo ">>> ${node_ip}" scp docker.service root@${node_ip}:/etc/systemd/system/
+  done
+
+# 配置和分发docker配置文件
+cat > docker-daemon.json <<EOF
+{
+    "registry-mirrors": ["https://hub-mirror.c.163.com", "https://docker.mirrors.ustc.edu.cn"],
+    "max-concurrent-downloads": 20
+}
+EOF
+
+for node_ip in ${NODE_IPS[@]}
+  do
+    echo ">>> ${node_ip}"
+    ssh root@${node_ip} "mkdir -p  /etc/docker/"
+    scp docker-daemon.json root@${node_ip}:/etc/docker/daemon.json
+  done
+
+# 启动docker服务
+for node_ip in ${NODE_IPS[@]}
+  do
+    echo ">>> ${node_ip}"
+    ssh root@${node_ip} "systemctl stop firewalld && systemctl disable firewalld"
+    ssh root@${node_ip} "/usr/sbin/iptables -F && /usr/sbin/iptables -X && /usr/sbin/iptables -F -t nat && /usr/sbin/iptables -X -t nat"
+    ssh root@${node_ip} "/usr/sbin/iptables -P FORWARD ACCEPT"
+    ssh root@${node_ip} "systemctl daemon-reload && systemctl enable docker && systemctl restart docker"
+    ssh root@${node_ip} 'for intf in /sys/devices/virtual/net/docker0/brif/*; do echo 1 > $intf/hairpin_mode; done'
+    ssh root@${node_ip} "sudo sysctl -p /etc/sysctl.d/kubernetes.conf"
+  done
+
+# 检查服务运行状态
+for node_ip in ${NODE_IPS[@]}
+  do
+    echo ">>> ${node_ip}"
+    ssh root@${node_ip} "systemctl status docker|grep Active"
+  done
---
+++ deployKube-proxy.sh
@@ -0,0 +1,122 @@
+#! /bin/bash
+
+cd /home/k8s-install
+source /usr/local/bin/environment.sh
+
+# 部署kube-proxy组件
+# 创建kube-proxy证书
+cat > kube-proxy-csr.json <<EOF
+{
+  "CN": "system:kube-proxy",
+  "key": {
+    "algo": "rsa",
+    "size": 2048
+  },
+  "names": [
+    {
+      "C": "CN",
+      "ST": "BeiJing",
+      "L": "BeiJing",
+      "O": "k8s",
+      "OU": "4Paradigm"
+    }
+  ]
+}
+EOF
+
+cfssl gencert -ca=/etc/kubernetes/cert/ca.pem \
+  -ca-key=/etc/kubernetes/cert/ca-key.pem \
+  -config=/etc/kubernetes/cert/ca-config.json \
+  -profile=kubernetes  kube-proxy-csr.json | cfssljson -bare kube-proxy
+
+# 创建和分发 kubeconfig 文件
+kubectl config set-cluster kubernetes \
+  --certificate-authority=/etc/kubernetes/cert/ca.pem \
+  --embed-certs=true \
+  --server=${KUBE_APISERVER} \
+  --kubeconfig=kube-proxy.kubeconfig
+
+kubectl config set-credentials kube-proxy \
+  --client-certificate=kube-proxy.pem \
+  --client-key=kube-proxy-key.pem \
+  --embed-certs=true \
+  --kubeconfig=kube-proxy.kubeconfig
+
+kubectl config set-context default \
+  --cluster=kubernetes \
+  --user=kube-proxy \
+  --kubeconfig=kube-proxy.kubeconfig
+
+kubectl config use-context default --kubeconfig=kube-proxy.kubeconfig
+
+for node_name in ${NODE_NAMES[@]}
+  do
+    echo ">>> ${node_name}"
+    scp kube-proxy.kubeconfig root@${node_name}:/etc/kubernetes/
+  done
+
+# 创建 kube-proxy 配置文件
+cat >kube-proxy.config.yaml.template <<EOF
+apiVersion: kubeproxy.config.k8s.io/v1alpha1
+bindAddress: ##NODE_IP##
+clientConnection:
+  kubeconfig: /etc/kubernetes/kube-proxy.kubeconfig
+clusterCIDR: ${CLUSTER_CIDR}
+healthzBindAddress: ##NODE_IP##:10256
+hostnameOverride: ##NODE_NAME##
+kind: KubeProxyConfiguration
+metricsBindAddress: ##NODE_IP##:10249
+mode: "ipvs"
+EOF
+
+for (( i=0; i < 3; i++ ))
+  do 
+    echo ">>> ${NODE_NAMES[i]}"
+    sed -e "s/##NODE_NAME##/${NODE_NAMES[i]}/" -e "s/##NODE_IP##/${NODE_IPS[i]}/" kube-proxy.config.yaml.template > kube-proxy-${NODE_NAMES[i]}.config.yaml
+    scp kube-proxy-${NODE_NAMES[i]}.config.yaml root@${NODE_NAMES[i]}:/etc/kubernetes/kube-proxy.config.yaml
+  done
+
+# 创建和分发 kube-proxy systemd unit 文件
+cat > kube-proxy.service <<EOF
+[Unit]
+Description=Kubernetes Kube-Proxy Server
+Documentation=https://github.com/GoogleCloudPlatform/kubernetes
+After=network.target
+
+[Service]
+WorkingDirectory=/var/lib/kube-proxy
+ExecStart=/usr/local/bin/kube-proxy \
+  --config=/etc/kubernetes/kube-proxy.config.yaml \
+  --alsologtostderr=true \
+  --logtostderr=false \
+  --log-dir=/var/log/kubernetes \
+  --v=2
+Restart=on-failure
+RestartSec=5
+LimitNOFILE=65536
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+for node_name in ${NODE_NAMES[@]}
+  do 
+    echo ">>> ${node_name}"
+    scp kube-proxy.service root@${node_name}:/etc/systemd/system/
+  done
+
+# 启动kube-proxy服务
+for node_ip in ${NODE_IPS[@]}
+  do
+    echo ">>> ${node_ip}"
+    ssh root@${node_ip} "mkdir -p /var/lib/kube-proxy"
+    ssh root@${node_ip} "mkdir -p /var/log/kubernetes"
+    ssh root@${node_ip} "systemctl daemon-reload && systemctl enable kube-proxy && systemctl restart kube-proxy"
+  done
+
+# 检查启动结果
+for node_ip in ${NODE_IPS[@]}
+  do
+    echo ">>> ${node_ip}"
+    ssh root@${node_ip} "systemctl status kube-proxy|grep Active"
+  done
